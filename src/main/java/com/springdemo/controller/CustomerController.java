6912180ce5c45adca94ca458f246709126b4bb50
ojuraev@hotmail.com <ojuraev@hotmail.com>
2021-12-23 11:19:36 -0600

    some updates

--- src/main/java/com/springdemo/controller/CustomerController.java
+++ src/main/java/com/springdemo/controller/CustomerController.java
@@ -1,15 +1,31 @@
 package com.springdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.springdemo.dao.CustomerDAO;
+import com.springdemo.entity.Customer;
+
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
-	
+
+	// need to inject our customer service
+	@Autowired
+	private CustomerDAO customerDAO;
+
 	@RequestMapping("/list")
 	public String listCustomers(Model theModel) {
+		
+		// get customers from the service
+		List<Customer> theCustomers = customerDAO.getCustomers();
+		
+		// add the customers to the model
+		theModel.addAttribute("customers", theCustomers);
 		
 		return "list-customers";
 	}
